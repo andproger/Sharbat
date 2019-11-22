8e1778bf52fba681d4d9e56a9d45df5701e3536d
Rich <beznika7890@mail.ru>
2019-11-22 16:27:06 +0300

    edit gradle

---
+++ app/src/main/java/com/example/sharbat/data/db/Event.kt
@@ -0,0 +1,3 @@
+package com.example.sharbat.data.db
+
+data class Event(val )
--- app/src/main/java/com/example/sharbat/presentation/ui/share/ShareFragment.kt
+++ app/src/main/java/com/example/sharbat/presentation/ui/share/ShareFragment.kt
@@ -1,4 +1,4 @@
-package com.example.sharbat.ui.home
+package com.example.sharbat.presentation.ui.share
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.sharbat.R
 
-class HomeFragment : Fragment() {
+class ShareFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var shareViewModel: ShareViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
+        shareViewModel =
+            ViewModelProviders.of(this).get(ShareViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_share, container, false)
+        val textView: TextView = root.findViewById(R.id.text_share)
+        shareViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
