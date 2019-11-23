ef9bc24443ec84a3c4b1a6c53f07e27f587b5ce1
Rich <beznika7890@mail.ru>
2019-11-23 12:50:07 +0300

    create toolbar title

--- app/src/main/java/com/example/sharbat/presentation/fetures/main/MainActivity.kt
+++ app/src/main/java/com/example/sharbat/presentation/fetures/main/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
         setupNavigation()
     }
 
